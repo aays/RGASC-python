4a5468500c81d2be3e6d8939ccadeebf69bd17b0
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2020-11-05 13:00:42 -0500

    Update fileparser.py

--- fileparser.py
+++ fileparser.py
@@ -1,14 +1,29 @@
-"""
-fileparser.py
-contains a file object class and more
-"""
+'''
+fileparser.py - contains basic functions to perform file operations
+'''
 
 class fo:
     def __init__(self, fname):
         self.fname = fname
         self.fname_length = len(fname)
+        self.fname_extension = self.get_extension(fname)
     
-    def get_extension(self):
-        i = self.fname.rfind('.')
-        extension = self.fname[i:]
+    def get_extension(self, fname):
+        # use this to introduce methods and attributes
+        i = fname.rfind('.')
+        extension = fname[i+1:]
         return extension
+    
+    def convert_to_list(self):
+        lines = []
+        with open(self.fname) as f:
+            for line in f:
+                lines.append(line.rstrip('\n'))
+        return lines
+    
+    def convert_to_dict(self):
+        dict_out = {}
+        with open(self.fname) as f:
+            for i, line in enumerate(f):
+                dict_out[i] = line.rstrip('\n')
+        return dict_out
